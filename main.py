ccc34e79ca075cd3ed05d21540aacbc9060e6cd4
mbardoe <mbardoe@gmail.com>
2022-03-25 06:19:12 -0400

    fixed formatting

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from math import sin, cos
 
 
 def create_velocities():
-    time =0
-    Delta_t=.1 #we need to increase our place on the circle
-    velocities = [] # We need a place to store the velocities
+    time = 0
+    Delta_t = .1  # we need to increase our place on the circle
+    velocities = []  # We need a place to store the velocities
     for i in range(100):
-        pass # remove this when you put in your code
+        pass  # remove this when you put in your code
         # create a tuple that gives the a velocity at the given time
 
         # add the velocity to the list
@@ -16,6 +16,8 @@ def create_velocities():
     # return the velocities
 
     return velocities
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print(create_velocities())
